d02ae97a2f2b2a17509619c4c174aa4db0f6f3a4
小霸王 <1005485265@qq.com>
2019-12-17 18:00:32 +0800

    Merge branch 'dev'
    # Conflicts:
    #	callable/src/com/yycompany/ABC.java

--- callable/src/com/yycompany/BQ.java
+++ callable/src/com/yycompany/BQ.java
@@ -1,9 +1,28 @@
 package com.yycompany;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class BQ {
-    public static void main(String[] args) {
-        ArrayBlockingQueue arrayBlockingQueue = new ArrayBlockingQueue(1);
+    public static void main(String[] args) throws InterruptedException {
+        ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue(3);
+//        System.out.println(arrayBlockingQueue.add(1));
+//        System.out.println(arrayBlockingQueue.add(2));
+//        System.out.println(arrayBlockingQueue.add(3));
+//        System.out.println(arrayBlockingQueue.add(4));
+        arrayBlockingQueue.offer(1, 2L, TimeUnit.SECONDS);
+        arrayBlockingQueue.offer(2, 4L, TimeUnit.SECONDS);
+        arrayBlockingQueue.offer(3, 6L, TimeUnit.SECONDS);
+        System.out.println("ddddddd");
+        arrayBlockingQueue.offer(4, 1L, TimeUnit.SECONDS);
+        System.out.println("ccccccc");
+        arrayBlockingQueue.poll();
+        arrayBlockingQueue.poll();
+        arrayBlockingQueue.poll();
+        System.out.println("aaaaaaaa");
+        arrayBlockingQueue.poll(5L,TimeUnit.SECONDS);
+        arrayBlockingQueue.offer(1);
+        System.out.println(arrayBlockingQueue.size());
+
     }
 }
